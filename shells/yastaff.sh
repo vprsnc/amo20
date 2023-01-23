0ca8ab5860a2ce99e3befe2d51c3128f376b1b7d
Georgy <heavypresence@riesup.net>
2023-01-23 14:28:34 +0300

    redoing statused

--- shells/yastaff.sh
+++ shells/yastaff.sh
@@ -2,6 +2,6 @@
 cd /home/analytics/amo20/
 # python yastaff_etl_leads.py
 # python yastaff_etl_calls.py
-# python yastaff_etl_statuses.py
+python yastaff_etl_statuses.py
 # python yastaff_etl_users.py
 # python yastaff_etl_pipelines.py
