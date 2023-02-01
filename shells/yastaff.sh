b1166f9954961a7183e2daf16d3002668f8619c9
Georgy <heavypresence@riesup.net>
2023-02-01 09:35:07 +0300

    etls loaded

--- shells/yastaff.sh
+++ shells/yastaff.sh
@@ -1,7 +1,7 @@
-#!/nix/store/j4nl4k97148p6kdkh2g2rvb3ab8847vf-python3-3.7.12-env/bin/python #TODO
-cd /home/georgy/Projects/python/amo20
-# python yastaff_etl_leads.py
+#!/home/analytics/amo20/venv/bin/python
+cd /home/analytics/amo20/
+python yastaff_etl_leads.py
 python yastaff_etl_calls.py
-# python yastaff_etl_statuses.py
-# python yastaff_etl_users.py
-# python yastaff_etl_pipelines.py
+python yastaff_etl_statuses.py
+python yastaff_etl_users.py
+python yastaff_etl_pipelines.py
