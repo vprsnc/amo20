08c23971a931b40e74523e7d41c077d1ec2dcca7
Georgy <heavypresence@riesup.net>
2023-02-01 09:33:14 +0300

    yastaff_statuses: read-write lastdate

--- shells/orps.sh
+++ shells/orps.sh
@@ -1,7 +1,7 @@
 #!/nix/store/j4nl4k97148p6kdkh2g2rvb3ab8847vf-python3-3.7.12-env/bin/python #TODO
 cd /home/georgy/Projects/python/amo20
-python orps_etl_leads.py
+# python orps_etl_leads.py
 # python orps_etl_calls.py
 # python orps_etl_statuses.py
 # python orps_etl_users.py
-# python orps_etl_pipelines.py
+python orps_etl_pipelines.py
